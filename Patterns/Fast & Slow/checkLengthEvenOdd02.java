94ca9e99d72f6da10f45d7a8483926c795000ee0
vaibhavv1207 <vaibhavx002@gmail.com>
2026-04-16 21:42:47 +0530

    Pattern Question Check Length of Linked List {ODD/EVEN}

--- Patterns/Fast & Slow/checkLengthEvenOdd02.java
+++ Patterns/Fast & Slow/checkLengthEvenOdd02.java
@@ -27,10 +27,9 @@ public class checkLengthEvenOdd02 {
                 this.next = null;
             }
         }
-        // Print Linked List
         void printList(){
-            if(head == null){
-                System.out.println("List is Empty: ");
+            if (head == null){
+                System.out.print("List is Empty: ");
                 return;
             }
             Node temp = head;
@@ -38,38 +37,36 @@ public class checkLengthEvenOdd02 {
                 System.out.print(temp.data+" -> ");
                 temp = temp.next;
             }
-            System.out.println("NULL");
+            System.out.print("NULL\n");
         }
-        void checkLength(){
+        String checkLength(){
             if (head == null){
-                System.out.println("List is Empty: ");
-                return;
+                return "Even";
             }
             Node fast = head;
             while (fast != null && fast.next != null){
                 fast = fast.next.next;
             }
             if (fast == null){
-                System.out.println("Even");
-                return;
+                return "Even";
             }
-            System.out.println("Odd");
-            
+            return "Odd";
         }
-    }   
-    public static void main(String args[]){
+    }
+    public static void main(String[] args) {
         LinkedList list = new LinkedList();
         LinkedList.Node n1 = new LinkedList.Node(10);
         LinkedList.Node n2 = new LinkedList.Node(20);
         LinkedList.Node n3 = new LinkedList.Node(30);
         LinkedList.Node n4 = new LinkedList.Node(40);
-        // Creating linked list: 10 -> 20 -> 30 -> 40
+        LinkedList.Node n5 = new LinkedList.Node(50);
+
         list.head = n1;
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
-
+        n4.next = n5;
         list.printList();
-        list.checkLength();
+        System.out.print("Length of LinkedList is: "+list.checkLength());
     }
 }
